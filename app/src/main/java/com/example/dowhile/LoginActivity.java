7c128f67e0ec548e6d89809c50e26bee043c9732
Gabriela <gabrielaoliveira30148@gmail.com>
2022-12-08 16:06:03 -0300

    tela login

---
+++ app/src/main/java/com/example/dowhile/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.dowhile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+}
