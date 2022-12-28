5f88582e1bea7472bcd44a51de0f12e5c689c1e2
Gabriela <gabrielaoliveira30148@gmail.com>
2022-12-28 17:33:58 -0300

    config farebase and login auth

--- app/src/main/java/com/example/dowhile/LoginActivity.java
+++ app/src/main/java/com/example/dowhile/LoginActivity.java
@@ -1,42 +1,151 @@
 package com.example.dowhile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "CustomAuthActivity";
+    private String mCustomToken;
+    private FirebaseAuth mAuth;
+
     private Button entrarLogin;
+    private EditText editEmail, editSenha;
+    private ProgressBar progressB;
+    String[] mensagem = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
         entrarLogin = findViewById(R.id.buttonEntrar);
+        editEmail = findViewById(R.id.email);
+        editSenha = findViewById(R.id.senha);
+        progressB = findViewById(R.id.progress);
+
         registrarEventos();
-
     }
-
     private void registrarEventos(){
-
         entrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                trocaActivEntrar();
-            }
 
+                String email = editEmail.getText().toString();
+                String senha = editSenha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagem[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }else{
+                    AutenticarUsuario();
+                }
+            }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
+
+    private void AutenticarUsuario() {
+
+        String email = editEmail.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            progressB.setVisibility(View.VISIBLE);
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    trocaActivEntrar();
+                                }
+                            },4000);
+
+                        } else {
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            System.out.println("ERRRRROOOOOOOO" + task.getException());
+                           /* Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();*/
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
+   /* private void AutenticarUsuario() {
+
+        String email = editEmail.getText().toString();
+        String senha = editSenha.getText().toString();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    progressB.setVisibility(View.VISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            trocaActivEntrar();
+                        }
+                    },4000);
+                }
+                }
+        });
+    }*/
+
 
     private void trocaActivEntrar(){
 
         Intent transJanela2 = new Intent(LoginActivity.this, TelaActivityCorr.class);
         startActivity(transJanela2);
-
+        finish();
     }
 }
