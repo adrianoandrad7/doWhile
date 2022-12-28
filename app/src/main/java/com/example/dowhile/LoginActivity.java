2edb69e3d4fb871f0bdb89cccbd84f613a6f7b55
Gabriela <gabrielaoliveira30148@gmail.com>
2022-12-28 17:52:29 -0300

    config Toast.makeText

--- app/src/main/java/com/example/dowhile/LoginActivity.java
+++ app/src/main/java/com/example/dowhile/LoginActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -16,13 +15,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import androidx.annotation.NonNull;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -30,20 +22,17 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "CustomAuthActivity";
-    private String mCustomToken;
     private FirebaseAuth mAuth;
 
     private Button entrarLogin;
     private EditText editEmail, editSenha;
     private ProgressBar progressB;
-    String[] mensagem = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-
         mAuth = FirebaseAuth.getInstance();
-
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
@@ -54,6 +43,7 @@ public class LoginActivity extends AppCompatActivity {
 
         registrarEventos();
     }
+
     private void registrarEventos(){
         entrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,11 +53,7 @@ public class LoginActivity extends AppCompatActivity {
                 String senha = editSenha.getText().toString();
 
                 if(email.isEmpty() || senha.isEmpty()){
-                    Snackbar snackbar = Snackbar.make(view, mensagem[0], Snackbar.LENGTH_SHORT);
-                    snackbar.setBackgroundTint(Color.WHITE);
-                    snackbar.setTextColor(Color.BLACK);
-                    snackbar.show();
-
+                    Toast.makeText(LoginActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
                 }else{
                     AutenticarUsuario();
                 }
@@ -86,64 +72,33 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void AutenticarUsuario() {
-
         String email = editEmail.getText().toString();
         String senha = editSenha.getText().toString();
-
         mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCustomToken:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                             progressB.setVisibility(View.VISIBLE);
-
                             new Handler().postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
                                     trocaActivEntrar();
                                 }
-                            },4000);
-
+                            },3000);
                         } else {
                             Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                            System.out.println("ERRRRROOOOOOOO" + task.getException());
-                           /* Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();*/
+                            System.out.println("ERRO" + task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
                     }
                 });
     }
-
-
-   /* private void AutenticarUsuario() {
-
-        String email = editEmail.getText().toString();
-        String senha = editSenha.getText().toString();
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if(task.isSuccessful()){
-
-                    progressB.setVisibility(View.VISIBLE);
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            trocaActivEntrar();
-                        }
-                    },4000);
-                }
-                }
-        });
-    }*/
-
-
     private void trocaActivEntrar(){
-
         Intent transJanela2 = new Intent(LoginActivity.this, TelaActivityCorr.class);
         startActivity(transJanela2);
         finish();
